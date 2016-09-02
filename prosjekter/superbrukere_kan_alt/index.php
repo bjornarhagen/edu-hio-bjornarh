f9de00bae41a4bcaed7e9cf9bd0c189f6b09fb37
Bear Hagen <post@bearhagen.com>
2016-09-02 08:28:36 +0200

    Add projects

---
+++ prosjekter/superbrukere_kan_alt/index.php
@@ -0,0 +1,22 @@
+<?php require_once('../../templates/head.php'); ?>
+</head>
+<body class="article light">
+    <?php require_once('../../templates/nav.php'); ?>
+    <article class="medium">
+        <header>
+            <h1>Superbrukere kan alt!</h1>
+            <p>Som IT-student regner alle med at man kan alt. Min GRIT gruppe jobbet sammen om 4 oppgaver.</p>
+        </header>
+        <section>
+            <h2>Oppgave 1-3</h2>
+            <p><a href="https://drive.google.com/open?id=0B3PL7sJb8hdkRm4tMWRteFFEOVE" target="_blank">PDF dokument</a>.
+            Oppgavene ble gjort i sammarbeid med Dataingenør gruppe 4 2016</p>
+        </section>
+        <section>
+            <h2>Oppgave 4</h2>
+            <p><a href="https://bearhagen.no/_/skole/hio/grit/2016-go-digital/bjornar_hagen-brukket-artikkel.pdf" target="_blank">Brukket artikkel</a></p>
+        </section>
+    </article>
+    <?php require_once('../../templates/footer.php'); ?>
+</body>
+</html>
