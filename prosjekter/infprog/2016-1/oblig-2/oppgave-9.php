f60d31f8be7527e3b20bac1a978c613496cc8ed3
Bear Hagen <post@bearhagen.com>
2016-10-03 16:10:14 +0200

    Added 9, 10 & 11

---
+++ prosjekter/infprog/2016-1/oblig-2/oppgave-9.php
@@ -0,0 +1,38 @@
+<?php require_once('../../../../templates/head.php'); ?>
+    <link rel="stylesheet" href="/~bjornarh/css/steps.css">
+    <link rel='stylesheet' href='/~bjornarh/fonts/hack/hack.css'>
+</head>
+<body>
+    <?php
+        $activeStep = 7;
+        require_once('steps.php');
+    ?>
+    <?php require_once('../../../../templates/nav.php'); ?>
+    <article>
+        <header id="intro">
+            <h1>Oppgave 7</h1>
+        </header>
+        <section id="result">
+            <h2>Resultat</h2>
+            <script>
+            </script>
+        </section>
+        <section id="code">
+            <h2>Koden</h2>
+            <h3>JavaScript</h3>
+            <pre class="language-javascript">
+                <code>
+                </code>
+            </pre>
+            <h3>HTML</h3>
+            <pre class="language-html">
+                <code>
+                </code>
+            </pre>
+        </section>
+    </article>
+    <?php require_once('../../../../templates/footer.php'); ?>
+    <script src="/~bjornarh/js/prism/prism.js"></script>
+    <link rel="stylesheet" href="/~bjornarh/css/prism/prism.css">
+</body>
+</html>
