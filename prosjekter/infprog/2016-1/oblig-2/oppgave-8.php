96b5f6594392245044260336526167626e71a23f
Bear Hagen <post@bearhagen.com>
2016-10-05 13:59:11 +0200

    Finished 8

--- prosjekter/infprog/2016-1/oblig-2/oppgave-8.php
+++ prosjekter/infprog/2016-1/oblig-2/oppgave-8.php
@@ -16,15 +16,15 @@
             <h2>Resultat</h2>
             <div class="row input-field">
                 <label for="o8-amount" class="col s1"><i class="icon-money"></i></label>
-                <input type="number" id="o8-amount" class="col s11" placeholder="Pengebeløp" value="10000">
+                <input type="number" id="o8-amount" class="col s11" placeholder="Pengebeløp">
             </div>
             <div class="row input-field">
                 <label for="o8-years" class="col s1"><i class="icon-calendar-2"></i></label>
-                <input type="number" id="o8-years" class="col s11" placeholder="Antall år" value="10">
+                <input type="number" id="o8-years" class="col s11" placeholder="Antall år">
             </div>
             <div class="row input-field">
                 <label for="o8-interest" class="col s1"><i class="icon-percent-up"></i></label>
-                <input type="number" id="o8-interest" class="col s11" placeholder="Rente" value="0.5">
+                <input type="number" id="o8-interest" class="col s11" placeholder="Rente">
             </div>
             <div class="row input-field center">
                 <button id="o8-calculate" class="btn black">Regn ut</button>
@@ -37,21 +37,19 @@
 
                 function ready() {
                     document.getElementById("o8-calculate").onclick = calculate;
-                    document.getElementById("o8-calculate").click();
                 }
 
                 function calculate() {
-                    console.log("asdf");
-                    var amount = document.getElementById("o8-amount").value;
-                    var years = document.getElementById("o8-years").value;
-                    var interest = document.getElementById("o8-interest").value;
+                    var amount = parseInt(document.getElementById("o8-amount").value);
+                    var years = parseInt(document.getElementById("o8-years").value);
+                    var interest = parseFloat(document.getElementById("o8-interest").value);
                     var output = document.getElementById("o8-output");
 
-                    for (var i = 0; i < years; i++) {
-                        var outputAmount = Math.floor(amount*(interest/100));
-                        amount = outputAmount;
+                    output.value = "";
 
-                        output.value += "Penger i banken: " + outputAmount + "\n";
+                    for (var i = 0; i < years; i++) {
+                        amount += (amount*interest)/100;
+                        output.value += "Penger i banken etter " + (i+1) + " år: " + amount.toFixed(2) + "\n"; // Output med 2 des
                     }
                 }
             </script>
