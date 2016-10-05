3439dad5d9992f4fe1f42af1d4258aae735f6c6f
Bear Hagen <post@bearhagen.com>
2016-10-05 13:59:25 +0200

    Finished 11

--- prosjekter/infprog/2016-1/oblig-2/oppgave-11.php
+++ prosjekter/infprog/2016-1/oblig-2/oppgave-11.php
@@ -1,20 +1,149 @@
 <?php require_once('../../../../templates/head.php'); ?>
     <link rel="stylesheet" href="/~bjornarh/css/steps.css">
     <link rel='stylesheet' href='/~bjornarh/fonts/hack/hack.css'>
+    <style>
+        #calendar {
+            display: block;
+            margin: 25px auto 0;
+            width: 50%;
+            line-height: 300%;
+            font-family: "Hack", monospace;
+        }
+
+        #calendar .cal-row {
+            width: 100%;
+            display: block;
+        }
+
+        #calendar .cal-row:first-child {
+            border-top: 1px solid black;
+            border-bottom: 1px solid black;
+        }
+
+        #calendar span {
+            transition: 0.5s;
+            display: inline-block;
+            text-align: center;
+        }
+
+        #calendar span:hover {
+            transition: 0.1s;
+            color: #fff;
+            background-color: #000;
+        }
+    </style>
 </head>
 <body>
     <?php
-        $activeStep = 7;
+        $activeStep = 11;
         require_once('steps.php');
     ?>
     <?php require_once('../../../../templates/nav.php'); ?>
     <article>
         <header id="intro">
-            <h1>Oppgave 7</h1>
+            <h1>Oppgave <?= $activeStep; ?></h1>
         </header>
         <section id="result">
             <h2>Resultat</h2>
+            <p>Du kan endre på kalenderen! Det er ikke mulig å ha mer enn 7 dager i uka.</p>
+            <div class="row input-field">
+                <label for="o11-cal-start" class="col s4">Først dag i uka</label>
+                <label for="o11-cal-days" class="col s4">Dager i måneden</label>
+                <label for="o11-cal-week" class="col s4">Dager i uka</label>
+            </div>
+            <div class="row input-field">
+                <select id="o11-cal-start" class="col s4">
+                    <option value="0">Mandag</option>
+                    <option value="1">Tirsdag</option>
+                    <option value="2">Onsdag</option>
+                    <option value="3">Torsdag</option>
+                    <option value="4">Fredag</option>
+                    <option value="5">Lørdag</option>
+                    <option value="6">Søndag</option>
+                </select>
+                <input type="number" id="o11-cal-days" class="col s4" min="7" value="30">
+                <input type="number" id="o11-cal-week" class="col s4" max="7" min="1" value="7">
+            </div>
+            <div id="calendar"></div>
             <script>
+                window.onload = ready;
+
+                function ready() {
+                    var weekStart = document.getElementById("o11-cal-start");
+                    var daysInMonth = document.getElementById("o11-cal-days");
+                    var daysInWeek = document.getElementById("o11-cal-week");
+                    
+                    setCalendar(daysInMonth.value, weekStart.value, daysInWeek.value);
+
+                    weekStart.oninput = function(){
+                        setCalendar(daysInMonth.value, this.value, daysInWeek.value)
+                    };
+
+                    daysInMonth.oninput = function(){
+                        setCalendar(this.value, weekStart.value, daysInWeek.value)
+                    };
+
+                    daysInWeek.oninput = function(){
+                        setCalendar(daysInMonth.value, weekStart.value, this.value)
+                    };
+                }
+
+                function setCalendar(daysInMonth, weekStart, daysInWeek) {
+                    var weekStart = parseInt(weekStart);
+                    var calendar = document.getElementById("calendar");
+                    var dayNames = ["Man", "Tir", "Ons", "Tor", "Fre", "Lør", "Søn"];
+                    var weekStartHTML = "<div class='cal-row'>";
+                    var weekEndHTML = "</div>";
+                    var tmpHTML = weekStartHTML;
+                    var extraStart = false;
+
+                    if (daysInWeek > 7 || daysInMonth <= 7) {
+                        return false;
+                    }
+
+                    calendar.innerHTML = ""; // Tøm kalender før start
+
+                    // Lag en rad med ukedagenes navn
+                    for (var i = 0; i < daysInWeek; i++) {
+                        var offset = Math.abs((i - weekStart) % dayNames.length);
+                        console.log(offset);
+
+                        tmpHTML += putDay(dayNames[offset]);
+
+                        if (dayNames.length === i) {
+                            i = 0;
+                        }
+                    }
+                    // Avslutt raden
+                    tmpHTML += weekEndHTML + weekStartHTML;
+                    
+                    // Lag alle dagene i månden
+                    for (var i = 1; i <= daysInMonth; i++) {
+                        if (extraStart === false) {
+                            extraStart = true;
+
+                            // Lag ekstra dager når uka starter på en annen dag enn mandag
+                            for (var j = daysInMonth-weekStart+1; j <= daysInMonth; j++) {
+                                tmpHTML += putDay(j);
+                            }
+                        }
+
+                        tmpHTML += putDay(i);
+
+                        // Avslatt og lag en ny rad for hver gjeldene dag er delelig med antall dager i uka
+                        if (i+weekStart % daysInWeek === 0) {
+                            tmpHTML += weekEndHTML + weekStartHTML;
+                        }
+                    }
+
+                    // Output HTML
+                    calendar.innerHTML = tmpHTML;
+
+                    // Wrap en dag i HTML.
+                    function putDay(day) {
+                        return "<span style='width:" + (100/daysInWeek) + "%'>" + day + "</span>";
+                    }
+                }
             </script>
         </section>
         <section id="code">
