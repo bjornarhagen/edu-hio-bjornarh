45dbaa40ce733c85fb3b5aa5a18edddc05bbf426
Bear Hagen <post@bearhagen.com>
2016-10-16 20:12:24 +0200

    Worked on task 5

--- prosjekter/infprog/2016-1/oblig-3/oppgave-5.php
+++ prosjekter/infprog/2016-1/oblig-3/oppgave-5.php
@@ -19,14 +19,52 @@
         <section id="result">
             <h2>Resultat</h2>
             <div class="row input-field">
-                <textarea id="o5-input" rows="3" class="col s12" placeholder="Skriv litt tekst">Dette dette er er en en ganske så enkel tekst da da</textarea>
+                <textarea id="o5-input" rows="3" class="col s12" placeholder="Skriv litt tekst">Abrakadabra å Dette dette er er en en ganske så  DettE enkel tekst da (asdf) dette da</textarea>
+            </div>
+            <div class="row">
+                <h3>Forekomster:</h3>
+                <ul id="o5-most-freq"></ul>
             </div>
             <script>
                 window.onload = ready;
 
                 function ready() {
                     var input = document.getElementById("o5-input");
-                    console.log(input.value.match(/[^\s]+/g)); // Matcher alle ord (også de med spesielle tegn)
+                    var wordsArray = input.value.match(/[^\s]+/g); // Matcher alle ord (også de med spesielle tegn)
+                    var wordList = {};
+
+                    // Loop gjennom alle ordene og tell hvor mange forkomster det er av hvert ord
+                    wordsArray.forEach(function(value, index){
+                        var word = value.toLowerCase();
+
+                        // Legger til ordet i en liste
+                        if (wordList[word]) {
+                            wordList[word] += 1;
+                        } else {
+                            wordList[word] = 1;
+                        }
+                    });
+
+                    var mostFreqList = document.getElementById("o5-most-freq");
+
+                    // Loop gjennom den nye listen. Legg til topp 10 i egen liste
+                    Object.keys(wordList).sort(function(a, b) {
+                        return (wordList[b] - wordList[a]); // Sorterer elementene i en synkende rekkefølge etter verdi
+                    }).forEach(function(item) {
+                        var tmpChild = document.createElement("li")
+                        tmpChild.appendChild(document.createTextNode(wordList[item] + ": " + item));
+
+                        tmpChild.style.backgroundColor = "red";
+                        tmpChild.style.width = wordList[item]*10 + "%";
+                        mostFreqList.appendChild(tmpChild);
+                    });
+
+                    Object.keys(wordList).sort(function(a, b) {
+                        return (b.length - a.length); // Sorterer elementene i en synkende rekkefølge etter lengde
+                    }).forEach(function(item) {
+                        // console.log(item);
+                    });
+
                 }
             </script>
         </section>
