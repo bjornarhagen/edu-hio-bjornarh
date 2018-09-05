e5f645f93ef18860283f4d16fb8299c476a1a389
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-09-05 22:58:54 +0200

    Navigation layout and styling (hidden for now)

--- partials/navigation.php
+++ partials/navigation.php
@@ -1,10 +1,12 @@
-<nav>
-  <section>
+<?php /*
+<nav id="nav-main">
+  <section class="logo">
     <a href="<?= $_project_path ?>"><?= $_project_name ?></a>
   </section>
-  <section>
+  <section class="links">
     <a href="<?= $_project_path ?>">Hjem</a>
     <a href="<?= $_project_path ?>/prosjekter">Prosjekter</a>
     <a href="<?= $_project_path ?>/om-meg">Om meg</a>
   </section>
 </nav>
+*/ ?>
