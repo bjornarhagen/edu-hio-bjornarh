db2575002fdaad90ccfe865063401868ff2f0856
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-09-10 14:01:39 +0200

    Added article (WIP)

---
+++ artikler/oppgave-2.php
@@ -0,0 +1,13 @@
+<?php
+  $body_id = 'article-oppgave-2';
+  $body_classes = 'article-page';
+?>
+<?php require('../partials/head.php'); ?>
+<?php require('../partials/navigation.php'); ?>
+<main>
+  <article>
+    <h1>Oppgave 2</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum tenetur, animi cum iure ipsum nihil.</p>
+  </article>
+</main>
+<?php require('../partials/footer.php'); ?>
