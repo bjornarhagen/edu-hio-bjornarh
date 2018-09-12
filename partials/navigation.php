5cf876e24ed77a0da00b10181d302758bb57ba8a
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-09-12 14:26:24 +0200

    Updated navigation

--- partials/navigation.php
+++ partials/navigation.php
@@ -2,14 +2,14 @@
   <nav id="nav-main">
     <div class="links links-left">
       <a href="<?= $_project_path ?>">Hjem</a>
-      <a href="<?= $_project_path ?>/om-meg">Om meg</a>
+      <a href="<?= $_project_path ?>/om-meg.php">Om meg</a>
     </div>
     <div class="logo">
       <a href="<?= $_project_path ?>"><?= $_project_name ?></a>
     </div>
     <div class="links links-right">
-      <a href="<?= $_project_path ?>/prosjekter">Obliger</a>
-      <a href="<?= $_project_path ?>/prosjekter">Lab</a>
+      <a href="<?= $_project_path ?>/obliger.php">Obliger</a>
+      <a href="<?= $_project_path ?>/lab.php">Lab</a>
     </div>
   </nav>
 </header>
